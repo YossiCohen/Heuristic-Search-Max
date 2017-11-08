f2a02993391ff0ad3cd69d7e334f0f54920ea6ef
Yossi Cohen <cohenyossi81@gmail.com>
2017-11-08 14:46:24 +0200

    Test added

--- GridTest/UntouchedAroundTheGoalHeuristic.cs
+++ GridTest/UntouchedAroundTheGoalHeuristic.cs
@@ -11,12 +11,14 @@ namespace GridTest
     {
         private static World _basicWorld;
         private static World _basicWorld2;
+        private static World _basicWorld3;
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context)
         {
             _basicWorld = new World(File.ReadAllText(@"..\..\Grid-10-6-5-4-0.grd"), new UntouchedAroundTheGoalHeuristic());
             _basicWorld2 = new World(File.ReadAllText(@"..\..\Grid-10-6-5-4-1.grd"), new UntouchedAroundTheGoalHeuristic());
+            _basicWorld3 = new World(File.ReadAllText(@"..\..\Clean_Grid_5x5BasicBlocked.grd"), new UntouchedAroundTheGoalHeuristic());
         }
 
         [TestMethod]
@@ -27,10 +29,17 @@ namespace GridTest
         }
 
         [TestMethod]
-        public void gValue_getFvalueForChildNode_Equals1()
+        public void gValue_getFvalueForChildNode_Equals24b()
         {
             GridSearchNode initialState = _basicWorld2.GetInitialSearchNode();
             Assert.AreEqual(24, initialState.h);
+        }
+
+        [TestMethod]
+        public void gValue_getFvalueForChildNode_Equals20()
+        {
+            GridSearchNode initialState = _basicWorld3.GetInitialSearchNode();
+            Assert.AreEqual(20, initialState.h);
         }
     }
 }
