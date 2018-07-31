89deba9b426dbc3f3cfec9ec1595c4fb34caaaa3
Yossi Cohen <cohenyossi81@gmail.com>
2018-07-31 04:32:38 +0300

    Basic skelton for MC based Max search

---
+++ GridTest/SimpleMonteCarloMaxTest.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grid.Domain;
+using MaxSearchAlg;
+
+namespace GridTest
+{
+    [TestClass]
+    public class SimpleMonteCarloMaxTest
+    {
+        private static World _basicWorld3X3;
+        private static World _basicWorld5X5Blocked;
+        private static World _basicWorld6X50;
+        private static World _basicWorld6X51;
+        private static World _basicWorld4X416;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            _basicWorld3X3 = new World(File.ReadAllText(@"..\..\33.grd"), new UntouchedAroundTheGoalHeuristic());
+            _basicWorld5X5Blocked = new World(File.ReadAllText(@"..\..\Clean_Grid_5x5BasicBlocked.grd"), new UntouchedAroundTheGoalHeuristic());
+            _basicWorld6X50 = new World(File.ReadAllText(@"..\..\Grid-10-6-5-4-0.grd"), new UntouchedAroundTheGoalHeuristic());
+            _basicWorld6X51 = new World(File.ReadAllText(@"..\..\Grid-10-6-5-4-1.grd"), new UntouchedAroundTheGoalHeuristic());
+            _basicWorld4X416 = new World(File.ReadAllText(@"..\..\Grid-20-4-4-2-16.grd"), new UntouchedAroundTheGoalHeuristic());
+        }
+
+        [TestMethod]
+        public void Create_NewDfBnbGrid3X3_findPath()
+        {
+            GridSearchNode initialState = _basicWorld3X3.GetInitialSearchNode<GridSearchNode>();
+            Solver solver = new SimpleMonteCarloMax(initialState, new GoalOnLocation(_basicWorld3X3.Goal));
+            Assert.IsNotNull(solver);
+            solver.Run(Int32.MaxValue); //Prevent stoping by time, should stop only when goal found
+            var maxGoal = solver.GetMaxGoal();
+            Assert.AreEqual(8, maxGoal.g);
+        }
+
+        [TestMethod]
+        public void Create_NewDfBnbGrid5X5WithBlocked_FindPath()
+        {
+            GridSearchNode initialState = _basicWorld5X5Blocked.GetInitialSearchNode<GridSearchNode>();
+            Solver solver = new SimpleMonteCarloMax(initialState, new GoalOnLocation(_basicWorld5X5Blocked.Goal));
+            Assert.IsNotNull(solver);
+            solver.Run(Int32.MaxValue);
+            var maxGoal = solver.GetMaxGoal();
+            Assert.AreEqual(20, maxGoal.g);
+        }
+
+    }
+}
---
+++ MaxSearchAlg/SimpleMonteCarloMax.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Common;
+
+namespace MaxSearchAlg
+{
+
+    public class SimpleMonteCarloMax : Solver
+    {
+        private INode _frontier;
+
+        public SimpleMonteCarloMax(INode initailNode, IGoalCheckMethod goalCheckMethod) : base(initailNode, new NoPrunning(), goalCheckMethod)
+        {
+            //TODO: Add SimNum,Add depth , Add Utility function
+            _frontier = initailNode;
+        }
+
+        /// <summary>
+        /// Moves the Monte Carlo algorithm forward one step.
+        /// </summary>
+        /// <returns>Returns the state the alorithm is in after the step, Searching or Ended.</returns>
+        internal override State Step()
+        {
+
+
+
+            // Check the frontier for valid goal
+            if (GoalCheckMethod.ValidGoal(_frontier))
+            {
+                candidateGoalNode = _frontier;
+                Log.WriteLineIf("[SimpleMonteCarloMax] Goal Found:" + candidateGoalNode, TraceLevel.Verbose);
+                return State.Ended;
+            }
+
+            //TODO: get legal childrens
+            //TODO: no legal childrens = return State.Ended
+
+
+
+            foreach (var child in _frontier.Children)
+            {
+                //TODO: Simulate until .depth 
+            }
+
+            //TODO: select best child and update frontier
+
+            return State.Ended; //TODO: this is set to ended so tests wount stuck - change it to searching
+        }
+
+    }
+
+}
