480f611d04f11343d06abb37c02a2583dc04c6d4
Yossi Cohen <cohenyossi81@gmail.com>
2018-08-05 02:22:08 +0300

    ExperimentEnv - simple pack

---
+++ rebuildExperimentEnv.bat
@@ -0,0 +1,13 @@
+rem Define VISUAL_STUDIO_PATH_DEVENV env. variable in your system, example:
+rem SET VISUAL_STUDIO_PATH_DEVENV=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe
+
+"%VISUAL_STUDIO_PATH_DEVENV%" HeuristicSearchMax.sln /rebuild Release 
+rmdir /S /Q ExperimentEnv
+mkdir ExperimentEnv
+copy Grid\bin\Release\*.exe ExperimentEnv
+copy Grid\bin\Release\*.config ExperimentEnv
+copy Grid\bin\Release\*.dll ExperimentEnv
+copy ExperimentRunner\bin\Release\*.exe ExperimentEnv
+copy ExperimentRunner\bin\Release\*.config ExperimentEnv
+copy ExperimentRunner\profile.txt ExperimentEnv
+copy FoldersExp.bat ExperimentEnv
