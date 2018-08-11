d9906910e594a07b768291e46072c82ddd32357e
Yossi Cohen <cohenyossi81@gmail.com>
2018-08-12 02:49:00 +0300

    RSD fixed for BCC Heuristic

--- GridTest/BiconnectedComponentsHeuristicTest.cs
+++ GridTest/BiconnectedComponentsHeuristicTest.cs
@@ -12,6 +12,7 @@ namespace GridTest
         private static World _basicWorldV1;
         private static World _basicWorldV1unTouched;
         private static World _basicWorldT1S0;
+        private static World _basicWorldBccAndPruningCheck;
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context)
@@ -21,6 +22,8 @@ namespace GridTest
             _basicWorldV1unTouched = new World(File.ReadAllText(@"..\..\Grid_5x5BCCHV1asReachable.grd"), 
                 new UntouchedAroundTheGoalHeuristic());
             _basicWorldT1S0 = new World(File.ReadAllText(@"..\..\Grid_5x5BccH_try1step0.grd"),
+                new BiconnectedComponentsHeuristic());
+            _basicWorldBccAndPruningCheck = new World(File.ReadAllText(@"..\..\Clean_Grid_5x5BccAndPruning.grd"),
                 new BiconnectedComponentsHeuristic());
         }
 
@@ -51,6 +54,59 @@ namespace GridTest
             AStarMax astar = new AStarMax(initialState, new GoalOnLocation(_basicWorldV1.Goal));
             Assert.IsNotNull(astar);
             var howEnded = astar.Run(Int32.MaxValue); 
+            var maxGoal = astar.GetMaxGoal();
+            Assert.IsNotNull(maxGoal);
+            Assert.AreEqual(State.Ended, howEnded);
+        }
+
+
+        [TestMethod]
+        public void AStarGrid_BasicRun_BCCHeuristicsBSD_Success()
+        {
+            GridSearchNode initialState = _basicWorldT1S0.GetInitialSearchNode<GridSearchNode>();
+            AStarMax astar = new AStarMax(initialState, new BasicSymmetryDetectionPrunning(), new GoalOnLocation(_basicWorldV1.Goal));
+            Assert.IsNotNull(astar);
+            var howEnded = astar.Run(Int32.MaxValue);
+            var maxGoal = astar.GetMaxGoal();
+            Assert.IsNotNull(maxGoal);
+            Assert.AreEqual(State.Ended, howEnded);
+        }
+
+        [TestMethod]
+        public void AStarGrid_BasicRun_BCCHeuristicsBSD_Success2()
+        {
+            GridSearchNode initialState = _basicWorldBccAndPruningCheck.GetInitialSearchNode<GridSearchNode>();
+            AStarMax astar = new AStarMax(initialState, new BasicSymmetryDetectionPrunning(), new GoalOnLocation(_basicWorldV1.Goal));
+            Assert.IsNotNull(astar);
+            var howEnded = astar.Run(Int32.MaxValue);
+            var maxGoal = astar.GetMaxGoal();
+            Assert.IsNotNull(maxGoal);
+            Assert.AreEqual(State.Ended, howEnded);
+        }
+
+        [TestMethod]
+        public void AStarGrid_BasicRun_BCCHeuristicsRSD_Success()
+        {
+            RsdGridSearchNode initialState = _basicWorldT1S0.GetInitialSearchNode<RsdGridSearchNode>();
+            ReachableSymmetryDetectionPrunning pruning  = new ReachableSymmetryDetectionPrunning();
+            AStarMax astar = new AStarMax(initialState, pruning, new GoalOnLocation(_basicWorldV1.Goal));
+            Assert.IsNotNull(astar);
+            pruning.setAstarOpenList(astar.OpenList);
+            var howEnded = astar.Run(Int32.MaxValue);
+            var maxGoal = astar.GetMaxGoal();
+            Assert.IsNotNull(maxGoal);
+            Assert.AreEqual(State.Ended, howEnded);
+        }
+
+        [TestMethod]
+        public void AStarGrid_BasicRun_BCCHeuristicsRSD_Success2()
+        {
+            RsdGridSearchNode initialState = _basicWorldBccAndPruningCheck.GetInitialSearchNode<RsdGridSearchNode>();
+            ReachableSymmetryDetectionPrunning pruning  = new ReachableSymmetryDetectionPrunning();
+            AStarMax astar = new AStarMax(initialState, pruning, new GoalOnLocation(_basicWorldV1.Goal));
+            Assert.IsNotNull(astar);
+            pruning.setAstarOpenList(astar.OpenList);
+            var howEnded = astar.Run(Int32.MaxValue);
             var maxGoal = astar.GetMaxGoal();
             Assert.IsNotNull(maxGoal);
             Assert.AreEqual(State.Ended, howEnded);
