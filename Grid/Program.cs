0e4ff9f36bb14e34488250094dfd30d83c35cf76
Yossi Cohen <cohenyossi81@gmail.com>
2018-09-07 02:23:38 +0300

    Version changed to 1.6
    This will be merged with the detailed log branch

--- Grid/Program.cs
+++ Grid/Program.cs
@@ -12,7 +12,7 @@ namespace Grid
     class Program
     {
 
-        private static readonly string VERSION = "1.5";
+        private static readonly string VERSION = "1.6";
         private static readonly string TIME_LIMIT = ConfigurationSettings.AppSettings["TimeLimit"] == null ? "120" : ConfigurationSettings.AppSettings["TimeLimit"];
         private static readonly string BCC_INIT = ConfigurationSettings.AppSettings["BccInit"] == null ? "true" : ConfigurationSettings.AppSettings["BccInit"];
 
